9d3faf8788fd85fb1e5b8e6a569e8c0b407211b7
Deepesh <sainideepesh12@gmail.com>
2025-11-11 15:47:56 +0530

    add path on branch5

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -61,7 +61,7 @@ function App() {
     <div className="min-h-screen flex items-center justify-center bg-gray-100 p-4">
       <div className="bg-white shadow-lg rounded-lg p-8 w-full max-w-md">
         <h1 className="text-2xl font-bold text-center text-gray-800 mb-6">
-          (React + Node + MongoDB Auth App) (AWS)
+          React + Node + MongoDB Auth App (AWS)
         </h1>
 
         {!token ? (
